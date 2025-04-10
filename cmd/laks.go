b5353bbe779878d327c1183a14d284e240b70f8d
Daniel Whitford <whitford.dsw@gmail.com>
2025-04-10 21:22:10 +0100

    update some non sense

--- cmd/laks.go
+++ cmd/laks.go
@@ -1,46 +1,52 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"io"
 	"os"
 
 	"github.com/danwhitford/laks"
 )
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-
-	fmt.Print(">>> ")
-	for scanner.Scan() { // Read each line
-		input := scanner.Bytes() // Get the scanned text
-		tokens, err := laks.Tokenise(input)
+	var f *os.File
+	
+	if len(os.Args) > 1 {
+		fname := os.Args[1]
+		var err error
+		f, err = os.Open(fname)
 		if err != nil {
 			panic(err)
 		}
-		fmt.Printf("\t%v\n", tokens)
-		exprs, err := laks.Parse(tokens)
-		if err != nil {
-			panic(err)
-		}
-		for _, e := range exprs {
-			fmt.Printf("\t%v\n", e)
-		}
-
-		bytecode, err := laks.Compile(exprs)
-		if err != nil {
-			panic(err)
-		}
-
-		err = laks.Run(bytecode, os.Stdout)
-		if err != nil {
-			panic(err)
-		}
-
-		fmt.Print(">>> ")
+	} else {
+		f = os.Stdin
+	}
+	
+	b, err := io.ReadAll(f)
+	if err != nil {
+		panic(err)
 	}
 
-	if err := scanner.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, "Error reading input:", err)
+	tokens, err := laks.Tokenise(b)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("\t%v\n", tokens)
+	exprs, err := laks.Parse(tokens)
+	if err != nil {
+		panic(err)
+	}
+	for _, e := range exprs {
+		fmt.Printf("\t%v\n", e)
+	}
+
+	bytecode, err := laks.Compile(exprs)
+	if err != nil {
+		panic(err)
+	}
+
+	err = laks.Run(bytecode, os.Stdout)
+	if err != nil {
+		panic(err)
 	}
 }
