465f517b12b43bb4dab9f06188d70479a70eb131
Daniel Whitford <whitford.dsw@gmail.com>
2025-05-29 23:35:11 +0100

    love semicolons

--- src/parser.rs
+++ src/parser.rs
@@ -32,28 +32,51 @@ fn parse(tokens: Vec<Token>) -> Vec<Stmt> {
     loop {
         match iter.peek() {
             Some(t) => match t {
-                Token::Int(_) => {
-                    exprs.push(Stmt::ExprStmt(parse_expr(&mut iter)))
-                }
+                Token::Int(_) => exprs.push(Stmt::ExprStmt(parse_expr(&mut iter))),
                 _ => panic!("dunno start of expression {:?}", t),
             },
             None => return exprs,
+        }
+        match iter.next() {
+            Some(t) => match t {
+                Token::Semi => (),
+                _ => panic!("wanted semicolon got {:?}", t),
+            },
+            None => panic!("wanted semicolon got EOF"),
         }
     }
 }
 
 fn parse_expr<'a>(iter: &mut std::iter::Peekable<impl Iterator<Item = &'a Token>>) -> Expr {
-    let mut expr = parse_literal(iter.next());
+    let mut expr = parse_expr2(iter);
 
     loop {
         match iter.peek() {
             Some(token) => match token {
                 Token::Plus | Token::Sub => {
                     let op = token_to_op(iter.next().unwrap());
+                    let right = parse_expr2(iter);
+                    expr = Expr::BinOp(op, Box::from(expr), Box::from(right));
+                }
+                _ => return expr,
+            },
+            None => return expr,
+        }
+    }
+}
+
+fn parse_expr2<'a>(iter: &mut std::iter::Peekable<impl Iterator<Item = &'a Token>>) -> Expr {
+    let mut expr = parse_literal(iter.next());
+
+    loop {
+        match iter.peek() {
+            Some(token) => match token {
+                Token::Mult | Token::Div => {
+                    let op = token_to_op(iter.next().unwrap());
                     let right = parse_literal(iter.next());
                     expr = Expr::BinOp(op, Box::from(expr), Box::from(right));
                 }
-                _ => (),
+                _ => return expr,
             },
             None => return expr,
         }
@@ -91,7 +114,7 @@ mod tests {
 
     #[test]
     fn test_simple() {
-        let got = parse(Vec::from([Token::Int(String::from("12"))]));
+        let got = parse(Vec::from([Token::Int(String::from("12")), Token::Semi]));
         let want = Vec::from([Stmt::ExprStmt(Expr::Lit(Value::IntVal(12)))]);
 
         assert_eq!(want, got);
@@ -103,11 +126,35 @@ mod tests {
             Token::Int(String::from("12")),
             Token::Plus,
             Token::Int(String::from("107")),
+            Token::Semi,
         ]));
         let want = Vec::from([Stmt::ExprStmt(Expr::BinOp(
             Operator::ADD,
             Box::from(Expr::Lit(Value::IntVal(12))),
             Box::from(Expr::Lit(Value::IntVal(107))),
+        ))]);
+
+        assert_eq!(want, got);
+    }
+
+    #[test]
+    fn test_precedence() {
+        let got = parse(Vec::from([
+            Token::Int(String::from("1")),
+            Token::Plus,
+            Token::Int(String::from("2")),
+            Token::Mult,
+            Token::Int(String::from("3")),
+            Token::Semi,
+        ]));
+        let want = Vec::from([Stmt::ExprStmt(Expr::BinOp(
+            Operator::ADD,
+            Box::from(Expr::Lit(Value::IntVal(1))),
+            Box::from(Expr::BinOp(
+                Operator::MUL,
+                Box::from(Expr::Lit(Value::IntVal(2))),
+                Box::from(Expr::Lit(Value::IntVal(3))),
+            )),
         ))]);
 
         assert_eq!(want, got);
